8e8492d3abf325836a8267b575b7b923f1c8bb27
dwija-dj <dwija2005@gmail.com>
2024-05-01 18:15:42 +0530

    changes to main

--- main.py
+++ main.py
@@ -31,3 +31,87 @@ async def read_about(request: Request):
 async def redirect_about():
     return RedirectResponse(url="/about")  # Redirect to the /about route
 
+
+@app.get("/booked", response_class=HTMLResponse)
+async def read_contact(request: Request):
+    # Render HTML template using Jinja
+    return templates.TemplateResponse("booked.html", {"request": request}) 
+
+@app.get("/booked.html", response_class=HTMLResponse)
+async def redirect_about():
+    return RedirectResponse(url="/booked")  # Redirect to the /about route
+
+
+@app.get("/bookshipment", response_class=HTMLResponse)
+async def read_contact(request: Request):
+    # Render HTML template using Jinja
+    return templates.TemplateResponse("bookshipment.html", {"request": request}) 
+
+@app.get("/bookshipment.html", response_class=HTMLResponse)
+async def redirect_about():
+    return RedirectResponse(url="/bookshipment")  # Redirect to the /about route
+
+
+@app.get("/create_account", response_class=HTMLResponse)
+async def read_contact(request: Request):
+    # Render HTML template using Jinja
+    return templates.TemplateResponse("create_account.html", {"request": request}) 
+
+@app.get("/create_account.html", response_class=HTMLResponse)
+async def redirect_about():
+    return RedirectResponse(url="/create_account")  # Redirect to the /about route
+
+
+@app.get("/forgetpwd", response_class=HTMLResponse)
+async def read_contact(request: Request):
+    # Render HTML template using Jinja
+    return templates.TemplateResponse("forgetpwd.html", {"request": request}) 
+
+@app.get("/forgetpwd.html", response_class=HTMLResponse)
+async def redirect_about():
+    return RedirectResponse(url="/forgetpwd")  # Redirect to the /about route
+
+
+@app.get("/login", response_class=HTMLResponse)
+async def read_contact(request: Request):
+    # Render HTML template using Jinja
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.get("/login.html", response_class=HTMLResponse)
+async def redirect_about():
+    return RedirectResponse(url="/login")  # Redirect to the /about route
+
+
+@app.get("/profile", response_class=HTMLResponse)
+async def read_contact(request: Request):
+    # Render HTML template using Jinja
+    return templates.TemplateResponse("profile.html", {"request": request}) 
+
+@app.get("/profile.html", response_class=HTMLResponse)
+async def redirect_about():
+    return RedirectResponse(url="/profile")  # Redirect to the /about route
+
+
+@app.get("/service", response_class=HTMLResponse)
+async def read_contact(request: Request):
+    # Render HTML template using Jinja
+    return templates.TemplateResponse("service.html", {"request": request}) 
+
+@app.get("/service.html", response_class=HTMLResponse)
+async def redirect_about():
+    return RedirectResponse(url="/service")  # Redirect to the /about route
+
+
+
+
+@app.get("/trackshipment", response_class=HTMLResponse)
+async def read_contact(request: Request):
+    # Render HTML template using Jinja
+    return templates.TemplateResponse("trackshipment.html", {"request": request}) 
+
+@app.get("/trackshipment.html", response_class=HTMLResponse)
+async def redirect_about():
+    return RedirectResponse(url="/trackshipment")  # Redirect to the /about route
+
+
+
